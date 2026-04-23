b9e02bade5252f7ac065d1f19b32531f4e9202a9
JR-Rossi <joaorb45@gmail.com>
2026-04-22 20:40:14 -0400

    primeiro commit

---
+++ exercicio1.js
@@ -0,0 +1,16 @@
+let nome = "ana";
+let idade = 10;
+let valorlogico = true;
+let a = "10";
+let b = 5;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(idade);
+console.log(typeof idade);
+
+console.log(valorlogico);
+console.log(typeof valorlogico);
+
+console.log(a + b);
